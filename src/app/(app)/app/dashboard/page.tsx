6e01d147ef4c488d33ae51478ca12e4133ffca34
swapnils118 <swapnil118@icloud.com>
2024-10-06 20:38:42 -0400

    Grid applied to dashboard page

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,4 +1,8 @@
 import Branding from "@/components/branding";
+import ContentBlock from "@/components/content-block";
+import PetDetails from "@/components/pet-details";
+import PetList from "@/components/pet-list";
+import SearchForm from "@/components/search-form";
 import Stats from "@/components/stats";
 
 export default function Page() {
@@ -8,6 +12,24 @@ export default function Page() {
         <Branding />
 
         <Stats />
+      </div>
+
+      <div className="grid grid-cols-3 grid-rows-[45px_1fr] gap-4 h-[600px]">
+        <div className="row-start-1 row-span-1 col-start-1 col-span-1">
+          <SearchForm />
+        </div>
+
+        <div className="row-start-2 row-span-full col-start-1 col-span-1">
+          <ContentBlock>
+            <PetList />
+          </ContentBlock>
+        </div>
+
+        <div className="row-start-1 row-span-full col-start-2 col-span-full">
+          <ContentBlock>
+            <PetDetails />
+          </ContentBlock>
+        </div>
       </div>
     </main>
   );
