cfa25a130b7c79fadd54160d9ef0628692140f32
swapnils118 <swapnil118@icloud.com>
2024-10-06 21:13:57 -0400

    Raw data added to pet list component

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -1,8 +1,32 @@
+import Image from "next/image";
+
 export default function PetList() {
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       <li>
-        <button>Pet List</button>
+        <button className="flex items-center gap-3 h-[70px] w-full cursor-pointer px-5 text-base hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">Benjamin</p>
+        </button>
+      </li>
+
+      <li>
+        <button className="flex items-center gap-3 h-[70px] w-full cursor-pointer px-5 text-base hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition ">
+          <Image
+            src="https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png"
+            alt="Pet image"
+            width={45}
+            height={45}
+            className="rounded-full object-cover"
+          />
+          <p className="font-semibold">German</p>
+        </button>
       </li>
     </ul>
   );
