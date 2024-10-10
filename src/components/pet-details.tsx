7ee7943ace5e5cb5ff07301a2172e2264a02654d
swapnils118 <swapnil118@icloud.com>
2024-10-10 17:41:48 -0400

    Refactoring of pet details component

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -7,39 +7,55 @@ export default function PetDetails() {
   const { selectedPet } = usePetContext();
   return (
     <section className=" flex flex-col w-full h-full">
-      <div className="flex items-center px-8  py-5 bg-white border-b border-black/[0.08]">
-        <Image
-          src={selectedPet?.imageUrl}
-          alt="Selected pet image"
-          width={75}
-          height={75}
-          className="rounded-full object-cover w-[75px] h-[75px]"
-        />
+      <TopBar pet={selectedPet} />
 
-        <h2 className="text-3xl font-semibold leading-7 ml-5">
-          {selectedPet?.name}
-        </h2>
+      <OtherInfo pet={selectedPet} />
+
+      <Notes pet={selectedPet} />
+    </section>
+  );
+}
+
+function TopBar({ pet }) {
+  return (
+    <div className="flex items-center px-8  py-5 bg-white border-b border-black/[0.08]">
+      <Image
+        src={pet?.imageUrl}
+        alt="Selected pet image"
+        width={75}
+        height={75}
+        className="rounded-full object-cover w-[75px] h-[75px]"
+      />
+
+      <h2 className="text-3xl font-semibold leading-7 ml-5">{pet?.name}</h2>
+    </div>
+  );
+}
+
+function OtherInfo({ pet }) {
+  return (
+    <div className="flex justify-around py-10 px-5 text-center">
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
+          Owner name
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.ownerName}</p>
       </div>
 
-      <div className="flex justify-around py-10 px-5 text-center">
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
-            Owner name
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
-        </div>
-
-        <div>
-          <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
-            Age
-          </h3>
-          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
-        </div>
+      <div>
+        <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
+          Age
+        </h3>
+        <p className="mt-1 text-lg text-zinc-800">{pet?.age}</p>
       </div>
+    </div>
+  );
+}
 
-      <section className=" flex-1 bg-white px-7 py-5 mx-8 mb-9 rounded-md border border-black/[0.08]">
-        {selectedPet?.notes}
-      </section>
+function Notes({ pet }) {
+  return (
+    <section className=" flex-1 bg-white px-7 py-5 mx-8 mb-9 rounded-md border border-black/[0.08]">
+      {pet?.notes}
     </section>
   );
 }
