a2d4e0bf9d9e0995fa2998e33c65b7e2513a522b
swapnils118 <swapnil118@icloud.com>
2024-10-09 22:50:24 -0400

    onClick attached on button in pet list

--- src/components/pet-list.tsx
+++ src/components/pet-list.tsx
@@ -4,12 +4,15 @@ import { usePetContext } from "@/lib/hooks";
 import Image from "next/image";
 
 export default function PetList() {
-  const { pets } = usePetContext();
+  const { pets, handleChangeSelectedPetId } = usePetContext();
   return (
     <ul className="bg-white border-b border-black/[0.08]">
       {pets.map((pet) => (
         <li key={pet.id}>
-          <button className="flex items-center gap-3 h-[70px] w-full cursor-pointer px-5 text-base hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition">
+          <button
+            onClick={handleChangeSelectedPetId(pet.id)}
+            className="flex items-center gap-3 h-[70px] w-full cursor-pointer px-5 text-base hover:bg-[#EFF1F2] focus:bg-[#EFF1F2] transition"
+          >
             <Image
               src={pet.imageUrl}
               alt="Pet image"
