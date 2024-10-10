8ef3cfcdcacdf1b6be9eb760e472159ef8f8af28
swapnils118 <swapnil118@icloud.com>
2024-10-10 17:19:35 -0400

    Styled pet details component

--- src/components/pet-details.tsx
+++ src/components/pet-details.tsx
@@ -6,7 +6,7 @@ import Image from "next/image";
 export default function PetDetails() {
   const { selectedPet } = usePetContext();
   return (
-    <section className="w-full h-full">
+    <section className=" flex flex-col w-full h-full">
       <div className="flex items-center px-8  py-5 bg-white border-b border-black/[0.08]">
         <Image
           src={selectedPet?.imageUrl}
@@ -20,6 +20,26 @@ export default function PetDetails() {
           {selectedPet?.name}
         </h2>
       </div>
+
+      <div className="flex justify-around py-10 px-5 text-center">
+        <div>
+          <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
+            Owner name
+          </h3>
+          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.ownerName}</p>
+        </div>
+
+        <div>
+          <h3 className="text-[13px] font-medium uppercase text-zinc-700 ">
+            Age
+          </h3>
+          <p className="mt-1 text-lg text-zinc-800">{selectedPet?.age}</p>
+        </div>
+      </div>
+
+      <section className=" flex-1 bg-white px-7 py-5 mx-8 mb-9 rounded-md border border-black/[0.08]">
+        {selectedPet?.notes}
+      </section>
     </section>
   );
 }
