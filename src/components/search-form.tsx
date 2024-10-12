f49b0c3eeb34cac0efbf5c6ebd4f70fe43a8a8f1
swapnils118 <swapnil118@icloud.com>
2024-10-11 23:53:22 -0400

    Added search-context-provider component

--- src/components/search-form.tsx
+++ src/components/search-form.tsx
@@ -4,6 +4,8 @@ export default function SearchForm() {
       <input
         className="w-full h-full bg-white/20 rounded-md px-5 outline-none transition focus:bg-white/50 hover:bg-white/30 placeholder:text-white/50"
         placeholder="Search pets"
+        type="search "
+        value={searchQuery}
       />
     </form>
   );
---
+++ src/contexts/search-context-provider.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { createContext, useState } from "react";
+
+type SearchContextProviderProps = {
+  children: React.ReactNode;
+};
+
+type TSearchContext = {
+  searchQuery: string;
+  handleChangeSearchQuery: (newValue: string) => void;
+};
+
+export const SearchContext = createContext<TSearchContext | null>(null);
+
+export default function SearchContextProvider({
+  children,
+}: SearchContextProviderProps) {
+  // State
+  const [searchQuery, setSearchQuery] = useState("");
+
+  // Derived State
+
+  // Event handlers / Actions
+  const handleChangeSearchQuery = (newValue: string) => {
+    setSearchQuery(newValue);
+  };
+
+  return (
+    <SearchContext.Provider value={{ searchQuery, handleChangeSearchQuery }}>
+      {children}
+    </SearchContext.Provider>
+  );
+}
