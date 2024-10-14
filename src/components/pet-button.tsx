af01c9079eef85cf2b5ad134f1bd3326eb792618
swapnils118 <swapnil118@icloud.com>
2024-10-13 20:26:48 -0400

    Dialog made dynamic for add and edit button

--- src/components/pet-button.tsx
+++ src/components/pet-button.tsx
@@ -1,7 +1,13 @@
 import { PlusIcon } from "@radix-ui/react-icons";
 import { Button } from "./ui/button";
 // import { Children } from "react";
-import { Dialog, DialogContent, DialogTrigger } from "./ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
 
 type PetButtonProps = {
   actionType: "add" | "edit" | "checkout";
@@ -14,31 +20,39 @@ export default function PetButton({
   onClick,
   children,
 }: PetButtonProps) {
-  {
-    if (actionType === "add") {
-      return (
-        <Dialog>
-          <DialogTrigger>
-            <Button size="icon">
-              <PlusIcon className="h-6 w-6" />
-            </Button>
-          </DialogTrigger>
-          <DialogContent>Add a new pet</DialogContent>
-        </Dialog>
-      );
-    }
-    if (actionType === "edit") {
-      return <Button variant="secondary">{children}</Button>;
-    }
-
-    if (actionType === "checkout") {
-      return (
-        <Button variant="secondary" onClick={onClick}>
-          {children}
-        </Button>
-      );
-    }
+  if (actionType === "checkout") {
+    return (
+      <Button variant="secondary" onClick={onClick}>
+        {children}
+      </Button>
+    );
   }
-  // If we leave off this return statement it will still work because we are taking care of all the possible scenarios in the above conditional statements
-  //   return <Button>Edit</Button>;
+
+  return (
+    <Dialog>
+      <DialogTrigger>
+        {actionType === "add" ? (
+          <Button size="icon">
+            <PlusIcon className="h-6 w-6" />
+          </Button>
+        ) : (
+          <Button variant="secondary">{children}</Button>
+        )}
+      </DialogTrigger>
+
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            {actionType === "add" ? "Add a new pet" : "Edit pet"}
+          </DialogTitle>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
+  );
 }
+// if (actionType === "edit") {
+//   return <Button variant="secondary">{children}</Button>;
+// }
+
+// If we leave off this return statement it will still work because we are taking care of all the possible scenarios in the above conditional statements
+//   return <Button>Edit</Button>;
