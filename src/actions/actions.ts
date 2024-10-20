e1718e1390c097cd4d8c487c9c6b7f35a1667666
swapnils118 <swapnil118@icloud.com>
2024-10-20 16:09:43 -0400

    checkoutPet server action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -51,3 +51,21 @@ export async function editPet(petId, formData) {
   }
   revalidatePath("/app", "layout");
 }
+
+export async function deletePet(petId) {
+  await sleep(2000);
+
+  try {
+    await prisma.pet.delete({
+      where: {
+        id: petId,
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Could not delete pet",
+    };
+  }
+
+  revalidatePath("/app", "layout");
+}
