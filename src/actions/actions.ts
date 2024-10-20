437d85b18f670f72531e359729b523c9fe77d527
swapnils118 <swapnil118@icloud.com>
2024-10-20 15:30:55 -0400

    editPet server action

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -26,3 +26,28 @@ export async function addPet(formData) {
 
   revalidatePath("/app", "layout");
 }
+
+export async function editPet(petId, formData) {
+  await sleep(2000);
+  try {
+    await prisma.pet.update({
+      where: {
+        id: petId,
+      },
+      data: {
+        name: formData.get("name"),
+        ownerName: formData.get("ownerName"),
+        age: parseInt(formData.get("age")),
+        imageUrl:
+          formData.get("imageUrl") ||
+          "https://bytegrad.com/course-assets/react-nextjs/pet-placeholder.png",
+        notes: formData.get("notes"),
+      },
+    });
+  } catch (error) {
+    return {
+      message: "Could not edit pet",
+    };
+  }
+  revalidatePath("/app", "layout");
+}
--- src/components/pet-form.tsx
+++ src/components/pet-form.tsx
@@ -5,7 +5,7 @@ import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
 import { Textarea } from "./ui/textarea";
-import { addPet } from "@/actions/actions";
+import { addPet, editPet } from "@/actions/actions";
 import PetFormBtn from "./pet-form-btn";
 import { toast } from "sonner";
 
@@ -47,10 +47,18 @@ export default function PetForm({
   return (
     <form
       action={async (formData) => {
-        const error = await addPet(formData);
-        if (error) {
-          toast.warning(error.message);
-          return;
+        if (actionType === "add") {
+          const error = await addPet(formData);
+          if (error) {
+            toast.warning(error.message);
+            return;
+          }
+        } else if (actionType === "edit") {
+          const error = await editPet(selectedPet?.id, formData);
+          if (error) {
+            toast.warning(error.message);
+            return;
+          }
         }
         onFormSubmission();
       }}
