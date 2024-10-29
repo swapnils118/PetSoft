819cef48b67926338b2585deeb45fc829c8ae9ef
swapnils118 <swapnil118@icloud.com>
2024-10-28 23:30:41 -0400

    auth pages done for both login and signup

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,3 +1,19 @@
+import AuthForm from "@/components/auth.form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Login Page</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5">Log In</H1>
+      <AuthForm />
+
+      <p className="mt-5 text-sm text-zinc-500">
+        No account yet?{" "}
+        <Link href="/signup" className="font-medium">
+          Sign up
+        </Link>
+      </p>
+    </main>
+  );
 }
--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,3 +1,19 @@
+import AuthForm from "@/components/auth.form";
+import H1 from "@/components/h1";
+import Link from "next/link";
+
 export default function Page() {
-  return <div>Signup Page</div>;
+  return (
+    <main>
+      <H1 className="text-center mb-5">Sign Up</H1>
+      <AuthForm />
+
+      <p className="mt-5 text-sm text-zinc-500">
+        Already have an account?{" "}
+        <Link href="/login" className="font-medium">
+          Login
+        </Link>
+      </p>
+    </main>
+  );
 }
