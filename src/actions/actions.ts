1ec2cc56e3cbe24c708f08fdc9415a7a106466a1
swapnils118 <swapnil118@icloud.com>
2024-10-28 21:49:18 -0400

    Server side validation with zod

--- src/actions/actions.ts
+++ src/actions/actions.ts
@@ -2,15 +2,23 @@
 import prisma from "@/lib/db";
 import { PetEssentials } from "@/lib/types";
 import { sleep } from "@/lib/utils";
+import { petFormSchema } from "@/lib/validations";
 import { Pet } from "@prisma/client";
 import { revalidatePath } from "next/cache";
 
 export async function addPet(pet: PetEssentials) {
   await sleep(1000);
 
+  const validatedPet = petFormSchema.safeParse(pet);
+  if (!validatedPet.success) {
+    return {
+      message: "Invalid pet data",
+    };
+  }
+
   try {
     await prisma.pet.create({
-      data: pet,
+      data: validatedPet.data,
     });
   } catch (error) {
     return {
