167c2fe4ad1fbcb49993dee28e063873f399f430
swapnils118 <swapnil118@icloud.com>
2024-10-29 23:47:40 -0400

    matcher added to middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,3 +4,7 @@ export function middleware(request: Request) {
   console.log(request.url);
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
