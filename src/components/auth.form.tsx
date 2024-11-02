d2cdca6a43f2f50286426e5b232db50ab26bd2e5
swapnils118 <swapnil118@icloud.com>
2024-11-01 23:04:52 -0400

    Auth button made dynamic

--- src/components/auth.form.tsx
+++ src/components/auth.form.tsx
@@ -1,8 +1,11 @@
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 import { Label } from "./ui/label";
+type AuthFormProps = {
+  type: "logIn" | "signUp";
+};
 
-export default function AuthForm() {
+export default function AuthForm({ type }: AuthFormProps) {
   return (
     <form>
       <div className="space-y-1">
@@ -14,7 +17,7 @@ export default function AuthForm() {
         <Label htmlFor="password">Password</Label>
         <Input id="password" type="password" />
       </div>
-      <Button>Log In</Button>
+      <Button>{type === "logIn" ? "Log In" : "Sign Up"}</Button>
     </form>
   );
 }
