06522c0677dd142c041cc95941e0bfb8a815d374
swapnils118 <swapnil118@icloud.com>
2024-11-02 18:53:18 -0400

    Credentials provider in NextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,10 +1,41 @@
 import NextAuth, { NextAuthConfig } from "next-auth";
+import Credentials from "next-auth/providers/credentials";
+import prisma from "./db";
+import bcrypt from "bcrypt";
 
 const config = {
   pages: {
     signIn: "/login",
   },
-  providers: [],
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        // runs on login
+        const { email, password } = credentials;
+
+        const user = await prisma.user.findUnique({
+          where: {
+            email,
+          },
+        });
+        if (!user) {
+          console.log("No user found");
+          return null;
+        }
+
+        const passwordsMatch = await bcrypt.compare(
+          password,
+          user.hashedPassword
+        );
+        if (!passwordsMatch) {
+          console.log("Invalid credentials");
+          return null;
+        }
+
+        return user;
+      },
+    }),
+  ],
   callbacks: {
     authorized: ({ request }) => {
       const isTryingToAccessApp = request.nextUrl.pathname.includes("/app");
