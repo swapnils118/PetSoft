a6b3e49ba149b3b7b5950c96f3b503037d42086e
swapnils118 <swapnil118@icloud.com>
2024-11-01 23:52:10 -0400

    NextAuth config

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,11 @@
-import { NextResponse } from "next/server";
+// export function middleware(request: Request) {
+//   console.log(request.url);
+//   return NextResponse.next();
+// }
 
-export function middleware(request: Request) {
-  console.log(request.url);
-  return NextResponse.next();
-}
+import { auth } from "./lib/auth";
+
+export default auth;
 
 export const config = {
   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
